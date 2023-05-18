f88f2d8b3a967c6ec6078a95dc5542d46c1a5773
Anushka-00 <anushkagupta102016@gmail.com>
2023-05-18 15:04:01 +0530

    convert main.py to python3

--- mininet_script/two_region.py
+++ mininet_script/two_region.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-__author__ = 'Zhang Shaojun'
 
 from mininet.net import Mininet
 from mininet.node import RemoteController, OVSSwitch
@@ -12,11 +11,11 @@ def two_region():
 
     net = Mininet(controller=RemoteController, switch=OVSSwitch, autoSetMacs=True)
 
-    print "*** Creating controllers"
+    print("*** Creating controllers")
     c1 = net.addController('c1', ip='192.168.124.128', port=6633)
     c2 = net.addController('c2', ip='192.168.124.129', port=6633)
 
-    print "*** Creating switches"
+    print("*** Creating switches")
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
     s3 = net.addSwitch('s3')
@@ -24,15 +23,15 @@ def two_region():
     s5 = net.addSwitch('s5')
     s6 = net.addSwitch('s6')
 
-    print "*** Creating hosts"
-    hosts1 = [net.addHost('h%d' % n) for n in 1, 2]
-    hosts2 = [net.addHost('h%d' % n) for n in 3, 4]
-    hosts3 = [net.addHost('h%d' % n) for n in 5, 6]
-    hosts4 = [net.addHost('h%d' % n) for n in 7, 8]
-    hosts5 = [net.addHost('h%d' % n) for n in 9, 10]
-    hosts6 = [net.addHost('h%d' % n) for n in 11, 12]
+    print("*** Creating hosts")
+    hosts1 = [net.addHost('h%d' % n) for n in (1, 2)]
+    hosts2 = [net.addHost('h%d' % n) for n in (3, 4)]
+    hosts3 = [net.addHost('h%d' % n) for n in (5, 6)]
+    hosts4 = [net.addHost('h%d' % n) for n in (7, 8)]
+    hosts5 = [net.addHost('h%d' % n) for n in (9, 10)]
+    hosts6 = [net.addHost('h%d' % n) for n in (11, 12)]
 
-    print "*** Creating links"
+    print("*** Creating links")
     for h in hosts1:
         net.addLink(s1, h)
     for h in hosts2:
@@ -59,13 +58,13 @@ def two_region():
             right_s = link.intf2.node
             right_p = right_s.ports[link.intf2]
             right_s_dpid = right_s.dpid
-            print "##### inter-region link from r1 to r2 is:"
-            print "      left switch:", left_s, "dpid:", left_s_dpid, "window port:", left_p
-            print "      right switch:", right_s, "dpid:", right_s_dpid, "window port:", right_p
-            print "      please modify the topo in cc_client and tflc"
+            print("##### inter-region link from r1 to r2 is:")
+            print("      left switch:", left_s, "dpid:", left_s_dpid, "window port:", left_p)
+            print("      right switch:", right_s, "dpid:", right_s_dpid, "window port:", right_p)
+            print("      please modify the topo in cc_client and tflc")
 
 
-    print "*** Starting network"
+    print("*** Starting network")
     net.build()
     c1.start()
     c2.start()
@@ -76,12 +75,13 @@ def two_region():
     s5.start([c2])
     s6.start([c2])
 
-    print "*** Running CLI"
+    print("*** Running CLI")
     CLI(net)
 
-    print "*** Stopping network"
+    print("*** Stopping network")
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel( 'info' )  # for CLI output
     two_region()
+
